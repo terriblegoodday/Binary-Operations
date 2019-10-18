5f4a5356d29db1bfa34782758870990b067efa73
Eduard <terriblegoodday@icloud.com>
2019-10-19 09:26:41 +1000

    Initial Commit

---
+++ Binary Operations/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Binary Operations
+//
+//  Created by Eduard Dzhumagaliev on 10/19/19.
+//  Copyright © 2019 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
